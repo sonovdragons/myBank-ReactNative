78a784515621721db6d09d097d818ea81d6c8088
sonovdragons <cagliolom@gmail.com>
2022-04-20 22:43:46 -0300

    colores del header

--- navigation/BankNavigator.js
+++ navigation/BankNavigator.js
@@ -6,13 +6,25 @@ import Scan from '../Screens/Scan'
 import SignUp from '../Screens/SignUp'
 import AccountMovements from '../Screens/AccountMovements'
 import BankCards from '../Screens/BankCards'
+import Colors from '../constants/color'
+import { Platform } from 'react-native'
 
 const Stack = createNativeStackNavigator()
 
 const BankNavigator = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
+            <Stack.Navigator initialRouteName="Home"
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+                },
+                headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+                headerTitleStyle: {
+                    fontWeight: 'bold'
+                }
+            }}
+            >
                 <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="Movimientos" component={AccountMovements} />
                 <Stack.Screen name="Tarjetas" component={BankCards} />
